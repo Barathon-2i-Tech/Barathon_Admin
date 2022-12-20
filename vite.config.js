727afcbfe924a005be8622ebd5d6e556d64ca6d3
eddymandran <eddy.mandran@gmail.com>
2022-12-20 09:05:49 +0000

    test(project): implement test configuration

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,12 @@ import svgr from 'vite-plugin-svgr';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [svgr(), react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/__tests__/setup.js',
+        coverage: {
+            reporter: ['text', 'json', 'html'],
+         }  
+    },
 });
